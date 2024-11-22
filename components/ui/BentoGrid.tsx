fc6d120cbe59f3ed22a0696d0d7d296aa3d8ede8
Fatma A <fka201520@gmail.com>
2024-11-23 01:14:18 +0300

    grid added

---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,64 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  spareImg,
+  titleClassName,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  id?: number | React.ReactNode;
+  img?: string | ReactNode;
+  imgClassName?: string | ReactNode;
+  spareImg?: string | ReactNode;
+  titleClassName?: string | ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        `row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl 
+        transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4`,
+        className
+      )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
+    >
+      <div className="group-hover/bento:translate-x-2 transition duration-200">
+        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          {title}
+        </div>
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ data/index.ts
@@ -0,0 +1,170 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+export const gridItems = [
+  {
+    id: 1,
+    title: "Empowering development through collaboration and innovation.",
+    description: "Passionate about adapting to evolving project requirements.",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Global collaboration with flexible communication.",
+    description: "Effectively collaborating across diverse time zones.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description:
+      "React, React Native, Redux Toolkit, MongoDB, Firebase, and more.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Bringing ideas to life with React and React Native.",
+    description:
+      "Creating user-friendly, responsive, and efficient applications.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Currently enhancing an event management app.",
+    description:
+      "Utilizing NativeWind, Redux Toolkit, and optimized workflows.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Let’s collaborate on something innovative.",
+    description: "I’m excited to hear your ideas!",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Social Event Application",
+    des: "A React Native-based social event app, featuring advanced state management using Redux Toolkit.",
+    img: "/p1.svg",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/redux.svg",
+      "/nativewind.svg",
+    ],
+    link: "/didiyos.app",
+  },
+  {
+    id: 2,
+    title: "E-commerce Application",
+    des: "An e-commerce app designed with React Native, optimized for performance and user experience.",
+    img: "/p2.svg",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/redux.svg",
+      "/firebase.svg",
+    ],
+    link: "/emaartechs.shop",
+  },
+  {
+    id: 3,
+    title: "Responsive Point of Sale System",
+    des: "A web and mobile POS system, built using React, Redux Toolkit, and MongoDB.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/mongo.svg", "/redux.svg", "/ts.svg"],
+    link: "/s-techs.pos",
+  },
+  {
+    id: 4,
+    title: "Interactive Coffee App",
+    des: "A responsive and visually appealing web app for coffee enthusiasts, built using React and Tailwind.",
+    img: "/p4.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg", "/css.svg"],
+    link: "/arabuleu.coffee",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Fatma is a remarkable developer with an eye for detail and the ability to bring innovative ideas to life. Her dedication to crafting user-friendly and engaging applications is impressive.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+export const companies = [
+  { id: 1, name: "Cloudinary", img: "/cloud.svg", nameImg: "/cloudName.svg" },
+  { id: 2, name: "Appwrite", img: "/app.svg", nameImg: "/appName.svg" },
+  { id: 3, name: "Hostinger", img: "/host.svg", nameImg: "/hostName.svg" },
+  { id: 4, name: "Stream", img: "/s.svg", nameImg: "/streamName.svg" },
+  { id: 5, name: "Docker", img: "/dock.svg", nameImg: "/dockerName.svg" },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "React Native Developer - Didiyos",
+    desc: "Developed a social event app using React Native and Redux Toolkit, optimizing the UI with NativeWind.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "React Native Developer - EmaarTechs",
+    desc: "Created an e-commerce app with advanced features like push notifications and caching.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "React Developer - Arabuleu",
+    desc: "Built interactive, user-friendly websites using React and modern styling libraries.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer - S-Techs",
+    desc: "Developed a POS system with a focus on responsiveness and intuitive user interfaces.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+export const socialMedia = [
+  { id: 1, img: "/git.svg" },
+  { id: 2, img: "/twit.svg" },
+  { id: 3, img: "/link.svg" },
+];
